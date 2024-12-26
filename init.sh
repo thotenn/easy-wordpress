fcd940c5139dcdc35e81b3a66d8ca4030d7f2c17
thotenn <josenriqueee@gmail.com>
2024-12-26 01:58:58 -0300

    .

--- init.sh
+++ init.sh
@@ -64,7 +64,7 @@ preflight_checks() {
 
     # Check disk space
     local free_space
-    free_space=$(df -k "$(dirname "$APP_PATH")" | tail -1 | awk '{print \$4}')
+    free_space=$(df -k "$(dirname "$APP_PATH")" | tail -1 | awk '{print $4}')
     if [ "$free_space" -lt "$REQUIRED_SPACE" ]; then
         log "Error: At least 5GB of free space is required"
         exit 1
